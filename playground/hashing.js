efa98e3561c038418462a2742ce7e7c4b8a593c9
maya <andreas.mayr@cable.vol.at>
2018-04-17 20:51:13 +0200

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,23 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+var password = '123abc!';
+
+/*bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
+*/
+
+var hashedPassword = '$2a$10$fmtDPcxc8yzFhlk4cCOzzeFjfBLgPUVweARVTMVeHOsbwFfy2UM9i';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+/*
 var data = {
     id: 10
 };
@@ -16,6 +33,8 @@ var hash = SHA256(message).toString();
 
 console.log(`Message: ${message}`);
 console.log(`Hash: ${hash}`);
+
+*/
 
 /*
 var data = {
