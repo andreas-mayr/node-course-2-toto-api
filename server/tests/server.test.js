3619d61f33369a20d505677466696efb1a4851fc
maya <andreas.mayr@cable.vol.at>
2018-04-19 17:27:02 +0200

    Add DELETE /users/me/oken route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -277,12 +277,31 @@ describe('POST /todos', () => {
                 User.findById(users[1]._id).then((user) => {
                     expect(user.tokens[0]).toInclude({access: 'auth', token: res.headers['x-auth']});
                     done();
-                });
-            }).catch((e) => done(e));
+                }).catch((e) => done(e));
+            });
         });
 
         it('shoud reject invalid login',  (done) => {
-            done();
+            request(app)
+            .post('/users/login')
+            .send({
+              email: users[1].email,
+              password: users[1].password + '1'
+            })
+            .expect(400)
+            .expect((res) => {
+              expect(res.headers['x-auth']).toNotExist();
+            })
+            .end((err, res) => {
+              if (err) {
+                return done(err);
+              }
+      
+              User.findById(users[1]._id).then((user) => {
+                expect(user.tokens.length).toBe(0);
+                done();
+              }).catch((e) => done(e));
+            });
         });
      });
 });
